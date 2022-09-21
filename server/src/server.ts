951556db69c1be1d129d77fee1aa5be6e3995291
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-09-20 21:53:01 -0300

    initializing

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import express, { json, request } from 'express'
+
+const app = express()
+
+// HTTP method / API RESTFUL / HTTP Codes
+
+app.get('/games', (req,res) => {
+  return res.json([]);
+});
+
+app.post('/ads', (req, res) => {
+  return res.json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+  return res.json([
+    { id: 1, name: 'Anúncio 1' },
+    { id: 2, name: 'Anúncio 2' },
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' }
+  ])
+})
+
+app.listen(3333)
