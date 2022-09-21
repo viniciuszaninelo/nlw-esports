e949f7f298c8b9d96c9437f9ef5bf1e981a4ccb7
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-09-20 23:11:39 -0300

    setting the background

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello word !!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,22 @@
+import { ImageBackground } from 'react-native';
+
+import BackgroundImg from '../../assets/background-galaxy.png'
+
+import { styles } from './styles'
+
+
+interface Props {
+  children: React.ReactNode
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground 
+      source={BackgroundImg}
+      defaultSource={BackgroundImg}
+      style={styles.container}
+    >
+      { children }
+    </ImageBackground>
+  );
+}
